849c7a11f8879a01487baa92b7511df7775e06b7
tyobaskara <prasetyaajibaskara@gmail.com>
2018-03-24 01:12:36 +0700

    h

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 const express = require('express')
 const next = require('next')
 
-const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
+const port = process.env.PORT || 3000
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare()
 .then(() => {
   const server = express()
-
-  // server.get('/a', (req, res) => {
-  //   return app.render(req, res, '/b', req.query)
-  // })
-
-  // server.get('/posts/:id', (req, res) => {
-  //   return app.render(req, res, '/posts', { id: req.params.id })
-  // })
 
   server.get('*', (req, res) => {
     return handle(req, res)
